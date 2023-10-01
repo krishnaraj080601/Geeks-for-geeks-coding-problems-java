aeb9ede8ae11b11296c01b085257112c7a151f35
krishnaraj G <141268650+krishnaraj080601@users.noreply.github.com>
2023-10-01 12:58:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of all prime numbers between 1 and N./sum-of-all-prime-numbers-between-1-and-n..java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            long ans = ob.prime_Sum(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    public long prime_Sum(int n)
+    {
+        // coint de here
+        	long sum=0;
+        for(int i=2;i<=n;i++){
+            boolean flag=true;
+            for(int j=2;j<=Math.sqrt(i);j++){
+                if(i%j==0){
+                    flag=false;
+                    break;
+                    
+                
+                }
+            
+            }
+                if(flag){
+                    sum=sum+i;
+                }
+            }
+        return sum;
+    }
+}
