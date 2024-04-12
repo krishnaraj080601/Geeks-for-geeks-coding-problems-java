b968f5217c52ab31e59b939edf6b12d79d34aec6
krishnaraj G <141268650+krishnaraj080601@users.noreply.github.com>
2024-04-12 23:54:36 +0530

    Updated Solution - GfG to GitHub

--- Easy/Square root of a number/square-root-of-a-number.java
+++ Easy/Square root of a number/square-root-of-a-number.java
@@ -29,7 +29,26 @@ class Solution
      long floorSqrt(long x)
 	 {
 		// Your code here
-		double z=Math.sqrt(x);
-		return (long)z;
+		long low=1;
+		long high=x;
+		long ans=-1;
+		while(low<=high){
+		    long mid=(low+high)/2;
+		    if(mid*mid==x){
+		        return mid;
+		    }
+		    long y=mid*mid;
+		    if(y<x){
+		        ans=mid;
+		        low=mid+1;
+		        
+		       
+		    }
+		    else{
+		        high=mid-1;
+		    }
+		}
+		return ans;
+		
 	 }
 }
