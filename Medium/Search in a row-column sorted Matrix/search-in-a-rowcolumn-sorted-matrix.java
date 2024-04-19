53831013afcb180ff31d211a3b4073b7ea1d65aa
krishnaraj G <141268650+krishnaraj080601@users.noreply.github.com>
2024-04-19 23:32:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search in a row-column sorted Matrix/search-in-a-rowcolumn-sorted-matrix.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG{
+	public static void main(String args[]) throws IOException { 
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t > 0){
+       		int rows=sc.nextInt();
+       		int columns=sc.nextInt();
+			
+			int matrix[][]=new int[rows][columns];
+          
+        	for(int i=0; i<rows;i++){            
+            	for(int j=0; j<columns;j++){
+                	matrix[i][j]=sc.nextInt();
+            	}
+         	}
+         	int target = sc.nextInt();
+         	
+			Solution x = new Solution();
+			
+			if (x.search(matrix, rows, columns, target)) 
+				System.out.println(1); 
+			else
+				System.out.println(0); 
+			t--;
+		}
+	} 
+}
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{ 
+    //Function to search a given number in row-column sorted matrix.
+	static boolean search(int matrix[][], int n, int m, int x) 
+	{  
+	    // code here 
+	    for(int i=0;i<n;i++){
+	        int low=0;
+	        int high=m-1;
+	        while(low<=high){
+	            int mid=(low+high)/2;
+	            if(matrix[i][mid]==x){
+	                return true;
+	            }
+	            if(matrix[i][mid]<x){
+	                low=mid+1;
+	                
+	            }
+	            else{
+	                high=mid-1;
+	            }
+	            
+	        }
+	        
+	    }
+	    return false;
+	} 
+} 
